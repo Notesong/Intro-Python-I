25a34e926587eed539202fcd3c6f45ecc8fb6488
Lee Damien <notesong.git@outlook.com>
2020-06-05 20:18:23 -0400

    10-12 complete

--- src/11_args.py
+++ src/11_args.py
@@ -5,14 +5,25 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x, y):
+    return x + y
+
 
 print(f1(1, 2))
+
 
 # Write a function f2 that takes any number of integer arguments and returns the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+# https://www.programiz.com/python-programming/function-argument
+def f2(*nums):
+    sum = 0
+    for num in nums:
+        sum += num
+    return sum
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,7 +33,8 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
+
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,6 +42,9 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(x, y=1):
+    return x + y
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +59,11 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+# https://treyhunner.com/2018/04/keyword-arguments-in-python/
+def f4(**args):
+    for param, value in args.items():
+        print(f'key: {param}, value: {value}')
+
 
 # Should print
 # key: a, value: 12
@@ -62,4 +82,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
